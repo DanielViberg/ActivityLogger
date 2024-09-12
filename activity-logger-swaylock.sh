e22a7ee0d0fb76332f0893b5026dcd12d714ea80
Daniel Viberg <daniel_viberg@outlook.com>
2024-09-12 15:24:11 +0200

    Added suspend after lock

--- activity-logger-swaylock.sh
+++ activity-logger-swaylock.sh
@@ -1,9 +1,11 @@
 swaylock &
+export PATH="/home/daniel/.local/bin:$PATH"
+/home/daniel/repos/DanielViberg/ActivityLogger/stop.sh
+systemctl suspend
 
 while pgrep -x swaylock >/dev/null; do
     sleep 1
 done
 
-sleep 2
-export PATH="/home/daniel/.local/bin:$PATH"
+sleep 0.1
 /home/daniel/repos/DanielViberg/ActivityLogger/activity_logger.sh start
